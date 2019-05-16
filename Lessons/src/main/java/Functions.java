b2dd11b0d9b3ee553d1d66b137e75378bff4381b
Vipok62 <50341248+Vipok62@users.noreply.github.com>
2019-05-16 23:37:54 +0300

    Create Functions.java

---
+++ Lessons/src/main/java/Functions.java
@@ -0,0 +1,10 @@
+public class Functions {
+    public static void main(String[] args){
+    hello("world");
+    }
+
+
+    private static void hello(String somebody){
+    System.out.println("Hello " + somebody + "!");
+        }
+}
