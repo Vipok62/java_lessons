5a14403ede0a14468ffea58e7efa7eeefdf3357e
Vipok62 <50341248+Vipok62@users.noreply.github.com>
2019-05-21 22:29:44 +0300

    21.05.19

--- Lessons/src/main/java/Functions.java
+++ Lessons/src/main/java/Functions.java
@@ -1,10 +1,22 @@
 public class Functions {
     public static void main(String[] args){
-    hello("world");
+        hello("world");
+
+        double i = 5;
+        System.out.println("Площадь квадрата со сторонами " + i + " равна " + area(i));
+
+        double a = 5;
+        double b = 7;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a,b));
+    }
+    public static void hello(String somebody){
+        System.out.println("Hello, " + somebody + "!");
+    }
+    public static double area(double i){
+        return i * i;
+    }
+    public static double area(double a, double b){
+        return a * b;
     }
 
-
-    private static void hello(String somebody){
-    System.out.println("Hello " + somebody + "!");
-        }
 }
